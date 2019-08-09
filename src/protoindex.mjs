77274aa94e6a016743abf37b68bc0dd1fb1124b0
David Itkin <david.itkin@gmail.com>
2019-08-09 07:21:52 -0400

    Add healthcheck route

--- src/protoindex.mjs
+++ src/protoindex.mjs
@@ -3,15 +3,17 @@ import session from 'express-session'
 import ws from 'ws'
 import body_parser from 'body-parser'
 import cookie_parser from 'cookie-parser'
-
+import empowerWithSocket from 'express-ws'
 // import mongo from 'mongodb'
 // import connect_mongo from 'connect-mongo'
 // import mongo_store from session
 
 import * as utils from './http_utils.mjs'
-import { startSocketServer } from './socket.mjs'
+import { setupSocket } from './protosocket.mjs'
 
-const server = express()
+const app = express()
+empowerWithSocket(app)
+
 const PORT = process.env.PORT || 1234
 
 // Setup middleware
@@ -42,10 +44,10 @@ const session_params = {
 }
 
 // Define middleware
-server.use(session(session_params))
+app.use(session(session_params))
 
 // Enable CORS
-server.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
     'chrome-extension://kdjokbdmplcfbigkcjekknnfboidokfo'
@@ -58,60 +60,34 @@ server.use((req, res, next) => {
   next()
 })
 
-server.post('/login', json_parser, (req, res, db) => {
+app.get('/', json_parser, (req, res, db) => {
+  console.log('healthcheck')
+  res.writeHead(200)
+
+  res.end('healthy')
+})
+
+app.post('/login', json_parser, (req, res, db) => {
   console.log('Logging In')
   utils.handle_login(req, res, db)
 })
 
-server.post('/deck', json_parser, (req, res, db) => {
+app.post('/deck', json_parser, (req, res, db) => {
   console.log('Creating Deck')
   utils.handle_new_deck(req, res, db)
 })
 
-server.get('/decks', json_parser, (req, res, db) => {
+app.get('/decks', json_parser, (req, res, db) => {
   console.log('Showing Decks')
   utils.handle_show_decks(req, res, db)
 })
 
-const wss = new ws.Server({ server })
-
-wss.on('connection', socket => {
-  console.log('Client connected')
-  socket.on('close', () =>
-    console.log('Client disconnected')
-  )
+app.ws('/', (ws, req) => {
+  setupSocket(ws)
 })
 
-server.listen(PORT, () =>
+app.listen(PORT, () =>
   console.log(
     `${new Date()} Server is listening on port ${PORT}`
   )
 )
-
-//   socket.on('message', event => {
-//     const data = JSON.parse(event)
-
-//     switch (data.type) {
-//       case 'JOIN_GAME':
-//         socket.id = Id.generate()
-//         const room_index = Room.getWithOpponent()
-//         if (room_index >= 0) {
-//           // room found
-//           socket.room = room_index
-//           Room.seatUser(room_index, socket.id)
-//           ws.start_game(room_index)
-//         } else {
-//           // room not found, creating
-//           ws.create_room(socket.id)
-//           socket.room = Room.getLast()
-//         }
-
-//         break
-//       case 'SEND_GAME_UPDATE':
-//         const { room } = socket
-//         delete data.type
-//         ws.update_room(room, data)
-//     }
-//   })
-// })
-// startSocketServer(server)
---
+++ src/protosocket.mjs
@@ -0,0 +1,90 @@
+import * as Board from './Board.mjs'
+import * as Room from './Room.mjs'
+import * as Id from './Id.mjs'
+
+export const setupSocket = ws => {
+  const is_ready = client =>
+    client.readyState === ws_dep.OPEN
+
+  ws.create_room = id => {
+    Room.create(id)
+    const body = { type: 'WAITING_ROOM', id }
+    ws.send_targeted([id], JSON.stringify(body))
+  }
+
+  ws.update_room = (room_id, board) => {
+    Room.set(room_id, { board })
+    const { ids } = Room.get(room_id)
+    const body = { ...board, type: 'GAME_UPDATED' }
+    ws.send_targeted(ids, JSON.stringify(body))
+  }
+
+  ws.start_game = room_index => {
+    const { ids } = Room.get(room_index)
+
+    const board = Board.getNew(ids)
+    Room.set(room_index, { board })
+
+    ws.send_start(ids)
+    const body = { ...board, type: 'GAME_UPDATED' }
+    ws.send_targeted(ids, JSON.stringify(body))
+  }
+
+  ws.send_start = function(ids) {
+    ;[...this.clients]
+      .filter(client => ids.includes(client.id))
+      .forEach(client => {
+        const body = {
+          type: 'GAME_START',
+          user: {
+            id: client.id,
+          },
+          opponent: {
+            id: Id.getOpponentId(ids, client.id),
+          },
+        }
+        client.send(JSON.stringify(body))
+      })
+  }
+
+  ws.send_targeted = (ids, message) =>
+    [...ws.clients]
+      .filter(client => ids.includes(client.id))
+      .forEach(client => {
+        client.send(message)
+      })
+
+  ws.onopen((event, ...args) => {
+    debugger
+    console.log('New connection!')
+  })
+
+  ws.onmessage((event, ...args) => {
+    debugger
+    const data = JSON.parse(event)
+
+    switch (data.type) {
+      case 'JOIN_GAME':
+        socket.id = Id.generate()
+        const room_index = Room.getWithOpponent()
+        if (room_index >= 0) {
+          // room found
+          socket.room = room_index
+          Room.seatUser(room_index, socket.id)
+          ws.start_game(room_index)
+        } else {
+          // room not found, creating
+          ws.create_room(socket.id)
+          socket.room = Room.getLast()
+        }
+
+        break
+      case 'SEND_GAME_UPDATE':
+        const { room } = socket
+        delete data.type
+        ws.update_room(room, data)
+    }
+  })
+
+  return ws
+}
